0a64cd66c1bb25cebadc0c209fa4ca389aadcfb2
Anjanikumar98 <anjanikumar2314@gmail.com>
2025-07-11 16:20:40 +0530

    Working on UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'details.dart';
+import 'home.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,12 +14,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-            backgroundColor: Colors.white,
-            body: Text('Anjanikumar ')
-        ),
-      ),
+      home: Home(),
+      routes: {'/details': (context) => Details()},
     );
   }
 }
+
+List<Map<String, dynamic>> habits = [
+  {'color': Colors.blue, 'title': 'YP', 'fulltext': 'Yoga Practice'},
+  {'color': Colors.red, 'title': 'GE', 'fulltext': 'Get Up Early'},
+  {'color': Colors.cyan, 'title': 'NS', 'fulltext': 'No Sugar'},
+];
+
+List<Map<String, dynamic>> habits2 = [
+  {
+    'color': Color(0xff7524ff),
+    'objectif': 'Learn 5 new words',
+    'progress': '5 from 7 this week',
+  },
+  {
+    'color': Color(0xfff03244),
+    'objectif': 'Get Up Early',
+    'progress': '5 from 7 this week',
+  },
+  {
+    'color': Color(0xff00d5e2),
+    'objectif': 'Create an App a day',
+    'progress': '6 from 7 this week',
+  },
+];
